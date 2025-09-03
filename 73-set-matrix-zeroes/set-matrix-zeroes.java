c85b6ef527f24d252a474a16c307f955a6e98385
DHANA-TEJA <137896864+DHANA-TEJA@users.noreply.github.com>
2025-09-03 11:34:08 +0530

    Time: 14 ms (4.56%) | Memory: 45.8 MB (47.13%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,25 +1,37 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        List<List<Integer>>  z = new ArrayList<>();
+        List<List<Integer>> z = new ArrayList<>();
 
-        for(int i = 0 ; i < matrix.length ; i++){
-            for( int j = 0 ; j < matrix[i].length; j++){
-                if(matrix[i][j] == 0){
-                    z.add(List.of(i,j));
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    z.add(List.of(i, j));
                 }
             }
         }
-        
-        for( List<Integer> x : z){
+
+
+        for (List<Integer> x : z) {
             int row = x.get(0);
             int col = x.get(1);
-            for(int i = 0 ; i < matrix.length ; i++){
-                for( int j = 0 ; j < matrix[i].length; j++){
-                matrix[row][j] = 0;
-                if( j < matrix.length){
-                    matrix[i][col] = 0;}
+            if (matrix.length != matrix[0].length) {
+                for (int i = 0; i < matrix.length; i++) {
+                    for (int j = 0; j < matrix[i].length; j++) {
+                        matrix[row][j] = 0;
+                        if (j < matrix.length) {
+                            matrix[i][col] = 0;
+                        }
+                    }
                 }
-            }}
+            } else {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    matrix[row][j] = 0;
+                    if (j < matrix.length) {
+                        matrix[j][col] = 0;
+                    }
+                }
+            }
+        }
             System.out.println(Arrays.deepToString(matrix));
             return;
     }
